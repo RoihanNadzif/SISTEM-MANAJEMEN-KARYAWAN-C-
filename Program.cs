f8606015716b321dc3399b4553f4062be5eb89f6
Ruyy <roihannadzif22@gmail.com>
2025-04-11 13:30:21 +0700

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -2,42 +2,42 @@
 
 internal class Program
 {
-    static void Main(string[] args)
+    static void Main(string[] args) // metod utama dari program
     {
-        Console.WriteLine("input jenis karyawan ketik angka saja(1tetap/2magang/3kontrak): ");
-        int input = int.Parse(Console.ReadLine());
+        Console.WriteLine("input jenis karyawan ketik angka saja(1tetap/2magang/3kontrak): "); //menampilkan kalimat ke konsol untuk meminta input jenis karyawan ketik....
+        int input = int.Parse(Console.ReadLine()); //membaca inputan pengguna,mengonversi string ke int menggunakan parse. Kemudian disimpan dalam variabel input.
 
-        Console.Write("input nama: ");
-        string nama = Console.ReadLine();
+        Console.Write("input nama: "); //menampilkan kalimat ke konsol untuk meminta input nama:
+        string nama = Console.ReadLine(); //membaca input nama dari user kemudian disimpan di variabel 'nama'
 
-        Console.Write("input ID: ");
-        string id = Console.ReadLine();
+        Console.Write("input ID: "); //menampilkan kalimat ke konsol untuk meminta input id:
+        string id = Console.ReadLine(); //membaca input id dari user kemudian disimpan di variabel 'id'
 
-        Console.Write("input gaji pokok: ");
-        double gajipokok = double.Parse(Console.ReadLine());
-
-        if (input == 1)
+        Console.Write("input gaji pokok: "); //menampilkan kalimat ke konsol untuk meminta input gajipokok:
+        double gajipokok = double.Parse(Console.ReadLine()); //membaca input gaji dari user kemudian dibuah ke tipe data double lalu disimpan di variabel 'gajipokok'
+        
+        if (input == 1) //cek if apakah user menginput 1
         {
-            KaryawanTetap karyawanTetap = new KaryawanTetap(nama, id, gajipokok);
-            double hasil = karyawanTetap.HitungGaji();
-            Console.WriteLine($"gaji akhirmu: {hasil}");
+            KaryawanTetap karyawanTetap = new KaryawanTetap(nama, id, gajipokok); // membuat objek baru karyawan tetap dengan 3 parameter utama : nama,id, dan gajipokok.
+            double hasil = karyawanTetap.HitungGaji(); // memanggil metod hitunggaji pada objek baru untuk menghitung hasil dan disimpan dalam variabel hasil
+            Console.WriteLine($"gaji akhirmu: {hasil}"); // menampilkan pesan ke konsol hasil perhitungan
         }
-        else if (input == 2)
+        else if (input == 2) //cek if apakah user menginput 2
         {
-            KaryawanKontrak karyawanKontrak = new KaryawanKontrak(nama, id, gajipokok);
-            double hasil = karyawanKontrak.HitungGaji();
-            Console.WriteLine($"gaji akhirmu: {hasil}");
+            KaryawanKontrak karyawanKontrak = new KaryawanKontrak(nama, id, gajipokok); // membuat objek baru karyawan kontrak dengan 3 parameter utama : nama,id, dan gajipokok.
+            double hasil = karyawanKontrak.HitungGaji(); // memanggil metod hitunggaji pada objek baru untuk menghitung hasil dan disimpan dalam variabel hasil
+            Console.WriteLine($"gaji akhirmu: {hasil}"); // menampilkan pesan ke konsol hasil perhitungan
         }
-        else if (input == 3)
+        else if (input == 3) //cek if apakah user menginput 3
         {
-            KaryawanMagang karyawanMagang = new KaryawanMagang(nama, id, gajipokok);
-            double hasil = karyawanMagang.HitungGaji();
-            Console.WriteLine($"gaji akhirmu: {hasil}");
+            KaryawanMagang karyawanMagang = new KaryawanMagang(nama, id, gajipokok); // membuat objek baru karyawan magang dengan 3 parameter utama : nama,id, dan gajipokok.
+            double hasil = karyawanMagang.HitungGaji(); // memanggil metod hitunggaji pada objek baru untuk menghitung hasil dan disimpan dalam variabel hasil
+            Console.WriteLine($"gaji akhirmu: {hasil}"); // menampilkan pesan ke konsol hasil perhitungan
         }
 
-        else
+        else // jika user memilih selain yang sudah ditentukan
         {
-            Console.WriteLine("inputan salah.");
+            Console.WriteLine("inputan salah."); //menampilkan pesan ke konsol inputan salah
         }
     }
 }
