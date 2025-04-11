8c537dcb28e0dac7fbb61597047ae8dafbc10b11
Ruyy <roihannadzif22@gmail.com>
2025-04-11 12:59:54 +0700

    Update KaryawanKontrak.cs

--- KaryawanKontrak.cs
+++ KaryawanKontrak.cs
@@ -6,15 +6,15 @@ using System.Threading.Tasks;
 
 namespace SISTEMMANAJEMENKARYAWAN
 {
-    class KaryawanKontrak : Karyawan1
+    class KaryawanKontrak : Karyawan1 //subclass dari kelas karyawan
     {
         public KaryawanKontrak(string nama, string iD, double gajipokok) : base(nama, iD, gajipokok)
-        { }
-        private double PKontrak = 200000;
-
-        public override double HitungGaji()
+        { } //construktor publik dari karyawankontrak dengan 3 parameter : nama,iD,gajipokok kemudian menggunakan base,
+        private double PKontrak = 200000; // memanggil kontruktor dari superclassnya dengan 3 parameternya ke konstruktor tersebut
+        //variable tipe data double PKontrak yang nilainya
+        public override double HitungGaji() // metod yang meng overide metod hitunggaji dari kelas karyawan
         {
-            return gajipokok + PKontrak;
+            return gajipokok + PKontrak; //menjumlahkan gajipokok dengan pkonrak dan nanti di return
         }
     }
 }
