0631f6979933c4f35c3642d7a24b3ada4df87879
Ruyy <roihannadzif22@gmail.com>
2025-04-11 13:10:04 +0700

    Update KaryawanTetap.cs

--- KaryawanTetap.cs
+++ KaryawanTetap.cs
@@ -6,14 +6,17 @@ using System.Threading.Tasks;
 
 namespace SISTEMMANAJEMENKARYAWAN
 {
-    class KaryawanTetap : Karyawan1
+    class KaryawanTetap : Karyawan1 //subclass dari kelas karyawan
     {
-        private double BTetap = 500000;
-        public KaryawanTetap(string Nama, string ID, double GajiPokok) : base(Nama, ID, GajiPokok){}
+        private double BTetap = 500000; //variable tipe data double BKontrak yang nilainya ditentukan oleh dalam kelas dan hanya bisa diakses di kelas tersebut.
+        public KaryawanTetap(string Nama, string ID, double GajiPokok) : base(Nama, ID, GajiPokok) //construktor publik dari karyawankontrak dengan 3 parameter : nama,iD,gajipokok kemudian menggunakan base,
+        {} // memanggil kontruktor dari superclassnya dengan 3 parameternya ke konstruktor tersebut
 
-        public override double HitungGaji()
+
+        public override double HitungGaji() // metod yang meng overide metod hitunggaji dari kelas karyawan
         {
-            return gajipokok + BTetap;
+            return gajipokok + BTetap;  //menjumlahkan gajipokok dengan BTetap dan nanti di return
+
         }
     }
 }
